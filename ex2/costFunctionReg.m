074b08a25dc5eec61485d7a38a1e70aa44882fb9
Abhilash Pal <abhilashpal8@gmail.com>
2017-10-10 13:50:41 +0530

    Add files via upload

---
+++ ex2/costFunctionReg.m
@@ -0,0 +1,36 @@
+function [J, grad] = costFunctionReg(theta, X, y, lambda)
+%COSTFUNCTION Compute cost and gradient for logistic regression
+%   J = COSTFUNCTION(theta, X, y) computes the cost of using theta as the
+%   parameter for logistic regression and the gradient of the cost
+%   w.r.t. to the parameters.
+
+% Initialize some useful values
+m = length(y); % number of training examples
+
+% You need to return the following variables correctly 
+J = 0;
+grad = zeros(size(theta));
+% ====================== YOUR CODE HERE ===========================
+% Instructions: Compute the cost of a particular choice of theta.
+%               You should set J to the cost.
+%               Compute the partial derivatives and set grad to the partial
+%               derivatives of the cost w.r.t. each parameter in theta
+%
+% Note: grad should have the same dimensions as theta
+%
+
+ht = @(X) sigmoid(X*theta);
+
+disp('===================================\nVALUE OF THETA(2:END,1) IS :\n========================================\n');
+theta(2:end,1)
+
+J = (1/m)*sum(-y.*log(ht(X))-(1.-y).*log(1.-ht(X)))+(lambda/(2*m))*(sum(theta(2:end,1).^2));
+
+%options = optimset('GradObj','on','MaxIter','100');
+%[optTheta,functionVal,exitflag]=fminunc(@costFunction,theta,options);
+grad(2:end) = ((1/m)*sum((ht(X).-y).*X(:,2:end)))'.+ (lambda/m)*theta(2:end);
+
+grad(1) = (1/m)*sum((ht(X).-y).*X(:,1));
+% ===================================================================
+
+end
---
+++ ex2/plotData.m
@@ -0,0 +1,36 @@
+function plotData(X, y)
+%PLOTDATA Plots the data points X and y into a new figure 
+%   PLOTDATA(x,y) plots the data points with + for the positive examples
+%   and o for the negative examples. X is assumed to be a Mx2 matrix.
+
+% Create New Figure
+figure; hold on;
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Plot the positive and negative examples on a
+%               2D plot, using the option 'k+' for the positive
+%               examples and 'ko' for the negative examples.
+%
+
+% To find indices of +ve and -ve examples : 
+pos = find(y==1);
+neg = find(y==0);
+
+
+% Plotting :
+plot(X(pos,1),X(pos,2),'kx','LineWidth',2,'MarkerSize',7,'MarkerFaceColor','y');
+plot(X(neg,1),X(neg,2),'ko','LineWidth',2,'MarkerSize',7,'MarkerFaceColor','g');
+
+
+
+
+
+
+
+% =========================================================================
+
+
+
+hold off;
+
+end
